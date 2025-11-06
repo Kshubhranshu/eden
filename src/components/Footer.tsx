29661fcf5320f5917189a79d9620316294a7f657
kumaar <shubhranshu14@gmail.com>
2025-11-06 09:52:27 +0530

    fixed the alignment issue

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -207,7 +207,7 @@ const Footer: React.FC = () => {
       <a
         href="https://wa.me/917533909333"
         target="_blank"
-        className="fixed bottom-6 right-6 bg-green-500 hover:bg-green-600 text-white rounded-full p-3 shadow-lg z-50 transition-all duration-300 hover:scale-110"
+        className="fixed bottom-6 left-6 bg-green-500 hover:bg-green-600 text-white rounded-full p-3 shadow-lg z-50 transition-all duration-300 hover:scale-110"
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
